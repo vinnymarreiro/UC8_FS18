0e6d00b3af04e4fefb128f4ac245e3459e23e2b8
Vinny Marreiro <107700261+vinnymarreiro@users.noreply.github.com>
2022-11-03 18:30:25 -0400

    Initial commit

---
+++ encontro_remoto_2_UC8.js
@@ -0,0 +1,8 @@
+//Percorrer de 0 até o número de alunos
+
+/*Se o número for par, escreva 'par' e o número correspondente.
+
+Se o número for impar, escreva 'impar' e o número correspondente.
+
+Se o número for zero, escreva 'zero' e o número correspondente.*/
+
