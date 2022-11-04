13ff7c75d6c7ee530c1bb9738213362b32b5f0d8
Vinny Marreiro <107700261+vinnymarreiro@users.noreply.github.com>
2022-11-03 20:08:12 -0400

    Fim da segunda parte

--- encontro_remoto_2_UC8.js
+++ encontro_remoto_2_UC8.js
@@ -8,12 +8,18 @@ Se o número for zero, escreva 'zero' e o número correspondente.*/
 
 let numeroDeAlunos = 10
 //contador++ é a mesma coisa que contador = contador + 1 / exemplo exemplo: x = 3, x = 3 + 1
-for (let contador = 0; contador <= numeroDeAlunos; contador++){
+let contador = 0
+while (contador <= numeroDeAlunos){
+    //console.log(contador)
     if (contador == 0){
         console.log("O número atual é zero")
-    }else if (contador % 2 == 0){
-    console.log(`O número ${contador} é par`)
+    }else if (contador % 2 != 0){
+    console.log(`O número ${contador} é impar`)
     }else{
-        console.log(`O número ${contador} é impar`)
+        console.log(`O número ${contador} é par`)
     }
+    contador++
 }
+
+///////////////////////////
+
